92ccd39a4f5a2deeffb30e91e637e8bb1b0f86b3
Greta Skipare <82839928+GretaSkip@users.noreply.github.com>
2023-02-03 22:47:40 +0000

    Initial commit with applicationUrl changes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReplitProjects.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
